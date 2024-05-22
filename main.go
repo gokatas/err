b41565179b634db7631ac9489ad49abc99b8f546
Jozef Reisinger <jreisinger@users.noreply.github.com>
2024-05-22 11:37:16 +0200

    Create main.go

---
+++ main.go
@@ -0,0 +1,47 @@
+// In Go, errors are values. That means they are not special and you can program
+// them. Here is one programming technique for avoiding repetitive error
+// handling. Adapted from https://go.dev/blog/errors-are-values.
+package main
+
+import (
+	"fmt"
+	"io"
+	"os"
+)
+
+type errReader struct {
+	r   io.Reader
+	err error
+}
+
+// read becomes a no-op as soon as an error
+// occurs but the error value gets saved
+func (er *errReader) read(buf []byte) {
+	if er.err != nil {
+		return
+	}
+	_, er.err = er.r.Read(buf)
+}
+
+func main() {
+	filename := "/etc/passwd"
+	if len(os.Args) > 1 {
+		filename = os.Args[1]
+	}
+
+	f, err := os.Open(filename)
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "errgo: %v\n", err)
+		os.Exit(1)
+	}
+
+	er := &errReader{r: f}
+	p := make([]byte, 9)
+	er.read(p[0:3]) // We do not
+	er.read(p[3:6]) // handle error
+	er.read(p[6:9]) // for each call.
+	if er.err != nil {
+		fmt.Fprintf(os.Stderr, "errgo: reading %s: %v\n", filename, er.err)
+		os.Exit(1)
+	}
+}
